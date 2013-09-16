3642972f4bfe1d05f1e3641e4372c01df23609de
Denys Kukushkin <denys.kukushkin@gmail.com>
2013-09-15 21:06:43 -0400

    MainWindow updates: Active Label, Inactive label addition and removal was implemented, also basic strength function construction

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include<QDebug>
+#include <QGraphicsScene>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -27,11 +28,29 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->activationSig_Remove_pushButton->setVisible(false);
     ui->transmissionSig_Add_pushButton->setVisible(false);
     ui->transmissionSig_Remove_pushButton->setVisible(false);
+    ui->graphicsView->setVisible(false);
 
+    //ui->graphicsView->setScene(new QGraphicsScene());
+
+    ui->radioButton_SideX->setChecked(true);
+    currentSide = 0;
+    selectedTile = 0;
+    /*QPainter paint;
+    paint.begin(ui->graphicsView);
+    paint.setPen(QPen(Qt::black, 4, Qt::SolidLine));
+    paint.drawLine(0,0,10,10);
+
+    paint.end();*/
 }
 
 MainWindow::~MainWindow()
 {
+    QObject::disconnect(ui->listWidget, SIGNAL(currentRowChanged(int )), this, SLOT(on_listWidget_currentRowChanged(int)));
+
+    for(QList<a_tile *>::iterator iter = tiles.begin(); iter != tiles.end(); ++iter)
+    {
+        delete (*iter);
+    }
     delete ui;
 }
 
@@ -65,7 +84,83 @@ void MainWindow::on_actionNew_Simulation_triggered()
 
 }
 
+void MainWindow::updateEntries(short side)
+{
+    if(selectedTile == 0) return;
+    //Clear all lists/tables
 
+    for(int i = ui->activeLabels_listWidget->count() - 1; i >=0; i--)
+    {
+        ui->activeLabels_listWidget->takeItem(i);
+    }
+
+    for(int i = ui->inactiveLabels_listWidget->count() - 1; i >=0; i--)
+    {
+        ui->inactiveLabels_listWidget->takeItem(i);
+    }
+
+    for(int i = ui->activationSignals_table->rowCount() - 1; i >=0; i--)
+    {
+        ui->activationSignals_table->takeItem(i, 0);
+        ui->activationSignals_table->takeItem(i, 1);
+
+        ui->activationSignals_table->removeRow(i);
+    }
+
+    for(int i = ui->transmissionSignals_table->rowCount() - 1; i >=0; i--)
+    {
+        ui->transmissionSignals_table->takeItem(i, 0);
+        ui->transmissionSignals_table->takeItem(i, 1);
+        ui->transmissionSignals_table->removeRow(i);
+    }
+
+    for(int i = ui->initiation_signals_tableWidget->rowCount() - 1; i >=0; i--)
+    {
+        ui->initiation_signals_tableWidget->takeItem(i, 0);
+        ui->initiation_signals_tableWidget->takeItem(i, 1);
+        ui->initiation_signals_tableWidget->removeRow(i);
+    }
+
+    //Insert values from current tile
+    for(QList<QListWidgetItem *>::const_iterator iter = selectedTile->activeLabels[side].begin();
+        iter != selectedTile->activeLabels[side].end(); iter++)
+    {
+        ui->activeLabels_listWidget->addItem(*iter);
+    }
+
+    for(QList<QListWidgetItem *>::const_iterator iter = selectedTile->inactiveLabels[side].begin();
+        iter != selectedTile->inactiveLabels[side].end(); iter++)
+    {
+        ui->inactiveLabels_listWidget->addItem(*iter);
+    }
+
+    for(QList<tablePair *>::const_iterator iter = selectedTile->activationSignals[side].begin();
+        iter != selectedTile->activationSignals[side].end(); iter++)
+    {
+        ui->activationSignals_table->insertRow(ui->activationSignals_table->rowCount());//insert row
+        //set items
+        ui->activationSignals_table->setItem(ui->activationSignals_table->rowCount() - 1, 0, (*iter)->first);
+        ui->activationSignals_table->setCellWidget(ui->activationSignals_table->rowCount() - 1, 1, (*iter)->second);
+    }
+
+    for(QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> >::const_iterator iter = selectedTile->transmissionSignals[side].begin();
+        iter != selectedTile->transmissionSignals[side].end(); iter++)
+    {
+        ui->transmissionSignals_table->insertRow(ui->transmissionSignals_table->rowCount());//insert row
+        //set items
+        ui->transmissionSignals_table->setItem(ui->transmissionSignals_table->rowCount() - 1, 0, (*iter).first);
+        ui->transmissionSignals_table->setItem(ui->transmissionSignals_table->rowCount() - 1, 1, (*iter).second);
+    }
+
+    for(QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> >::const_iterator iter = selectedTile->initiationSignals.begin();
+        iter != selectedTile->initiationSignals.end(); iter++)
+    {
+        ui->initiation_signals_tableWidget->insertRow(ui->initiation_signals_tableWidget->rowCount());//insert row
+        //set items
+        ui->initiation_signals_tableWidget->setItem(ui->initiation_signals_tableWidget->rowCount() - 1, 0, (*iter).first);
+        ui->initiation_signals_tableWidget->setItem(ui->initiation_signals_tableWidget->rowCount() - 1, 1, (*iter).second);
+    }
+}
 
 void MainWindow::on_NewTileButton_clicked()
 {
@@ -73,8 +168,9 @@ void MainWindow::on_NewTileButton_clicked()
 
     /*QListWidgetItem item("Tile " + QString::number(this->currentTile));
     item.setFlags(item.flags() | Qt::ItemIsEditable);*/
-
-    ui->listWidget->addItem(QString("Tile ") + QString::number(this->currentTile)); //add tile to the list
+    tiles.append(new a_tile(QString("Tile ") + QString::number(this->currentTile)));
+    tiles.last()->Tile->setFlags(tiles.last()->Tile->flags() | Qt::ItemIsEditable);
+    ui->listWidget->addItem(tiles.last()->Tile); //add tile to the list
 
     ui->tile_comboBox->addItem(QString("Tile ") + QString::number(this->currentTile));  //add tile to the combo box
 
@@ -93,10 +189,12 @@ void MainWindow::on_DeleteTileButton_clicked()
     else    //if it does
     {
         qDebug()<<"Selected item: "<<ui->listWidget->currentItem()->text()<<"\n";
-
-        ui->tile_comboBox->removeItem(ui->listWidget->currentRow());    //remove item from the combo box
-
-        delete ui->listWidget->takeItem(ui->listWidget->currentRow());   //delete it from the list
+        int row = ui->listWidget->currentRow();
+        a_tile * toRemove = tiles.at(row); //get pointer to the item to remove
+        tiles.removeOne(toRemove);  //remove item from the list of tiles
+        ui->tile_comboBox->removeItem(row);    //remove item from the combo box
+        ui->listWidget->takeItem(row);   //delete it from the list widget
+        delete toRemove;  //remove item
 
     }
 
@@ -134,6 +232,8 @@ void MainWindow::on_tabWidget_currentChanged(int index)
         ui->activationSig_Remove_pushButton->setVisible(true);
         ui->transmissionSig_Add_pushButton->setVisible(true);
         ui->transmissionSig_Remove_pushButton->setVisible(true);
+
+        ui->graphicsView->setVisible(false);
         break;
     case 1:
         qDebug()<<"Index = 1\n";
@@ -158,6 +258,8 @@ void MainWindow::on_tabWidget_currentChanged(int index)
         ui->activationSig_Remove_pushButton->setVisible(true);
         ui->transmissionSig_Add_pushButton->setVisible(true);
         ui->transmissionSig_Remove_pushButton->setVisible(true);
+
+        ui->graphicsView->setVisible(false);
         break;
     case 2:
         qDebug()<<"Index = 2\n";
@@ -181,6 +283,9 @@ void MainWindow::on_tabWidget_currentChanged(int index)
         ui->activationSig_Remove_pushButton->setVisible(false);
         ui->transmissionSig_Add_pushButton->setVisible(false);
         ui->transmissionSig_Remove_pushButton->setVisible(false);
+
+
+        ui->graphicsView->setVisible(true);
         break;
     case 3:
         qDebug()<<"Index = 3\n";
@@ -204,8 +309,10 @@ void MainWindow::on_tabWidget_currentChanged(int index)
         ui->activationSig_Remove_pushButton->setVisible(false);
         ui->transmissionSig_Add_pushButton->setVisible(false);
         ui->transmissionSig_Remove_pushButton->setVisible(false);
+
+        ui->graphicsView->setVisible(true);
         break;
-    case 4:
+   /* case 4:
         qDebug()<<"Index = 4\n";
         ui->radioButton_SideX->setVisible(false);
         ui->radioButton_SideY->setVisible(false);
@@ -227,9 +334,215 @@ void MainWindow::on_tabWidget_currentChanged(int index)
         ui->activationSig_Remove_pushButton->setVisible(false);
         ui->transmissionSig_Add_pushButton->setVisible(false);
         ui->transmissionSig_Remove_pushButton->setVisible(false);
-        break;
+        break;*/
     default:
         qDebug()<<"default: Index = "<<index<<"\n";
         break;
     }
+}
+
+void MainWindow::on_listWidget_clicked(const QModelIndex &index)
+{
+    qDebug()<<"Row clicked: "<<index.row();
+    if(index.row() < 0) return;
+    int currentRow = index.row();
+    ui->tile_comboBox->setCurrentIndex(currentRow); //update combobox
+    selectedTile = tiles.at(currentRow);    //update selectedTile value
+    ui->radioButton_SideX->setChecked(true);    //select side X by default
+    currentSide = 0;
+    updateEntries(currentSide);    //update all lists and tables
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    close();
+}
+
+void MainWindow::on_activeLabel_Add_pushButton_clicked()
+{
+    qDebug()<<"Add active label button pressed";
+
+    if(selectedTile == false) return;   //if no tile is selected return
+
+    selectedTile->activeLabels[currentSide].append(new QListWidgetItem("0"));
+
+    //make entries editable
+    selectedTile->activeLabels[currentSide].last()->setFlags(selectedTile->activeLabels[currentSide].last()->flags() | Qt::ItemIsEditable);
+    ui->activeLabels_listWidget->addItem(selectedTile->activeLabels[currentSide].last());
+
+
+}
+
+
+void MainWindow::on_activeLabel_Remove_pushButton_clicked()
+{
+    qDebug()<<"Remove active label button pressed";
+    if(selectedTile == 0) return;   //if no tile is selected return
+
+    if(ui->activeLabels_listWidget->currentItem() == 0)  //check if label exist
+    {
+        qDebug()<<"No item was selected\n";
+    }
+    else    //if it does
+    {
+        qDebug()<<"Selected item: "<<ui->activeLabels_listWidget->currentItem()->text()<<"\n";
+
+        QListWidgetItem * toRemove = selectedTile->activeLabels[currentSide].at(ui->activeLabels_listWidget->currentRow());
+        selectedTile->activeLabels[currentSide].removeAt(ui->activeLabels_listWidget->currentRow()); //delete it from the tile list
+
+        ui->activeLabels_listWidget->takeItem(ui->activeLabels_listWidget->currentRow());   //delete it from the widget list
+        delete toRemove;
+    }
+}
+
+void MainWindow::on_inactiveLabel_Add_pushButton_clicked()
+{
+    qDebug()<<"Add inactive label button pressed";
+
+    if(selectedTile == 0) return;   //if no tile is selected return
+
+    selectedTile->inactiveLabels[currentSide].append(new QListWidgetItem("0"));
+
+    //make entries editable
+    selectedTile->inactiveLabels[currentSide].last()->setFlags(selectedTile->inactiveLabels[currentSide].last()->flags() | Qt::ItemIsEditable);
+    ui->inactiveLabels_listWidget->addItem(selectedTile->inactiveLabels[currentSide].last()); //add item to the list widget
+}
+
+void MainWindow::on_inactiveLabel_Remove_pushButton_clicked()
+{
+    qDebug()<<"Remove inactive label button pressed";
+    if(ui->inactiveLabels_listWidget->currentItem() == 0)  //check if label exist
+    {
+        qDebug()<<"No item was selected\n";
+    }
+    else    //if it does
+    {
+        qDebug()<<"Selected item: "<<ui->inactiveLabels_listWidget->currentItem()->text()<<"\n";
+
+        QListWidgetItem * toRemove = selectedTile->inactiveLabels[currentSide].at(ui->inactiveLabels_listWidget->currentRow());
+        selectedTile->inactiveLabels[currentSide].removeAt(ui->inactiveLabels_listWidget->currentRow()); //delete it from the tile list
+
+        ui->inactiveLabels_listWidget->takeItem(ui->inactiveLabels_listWidget->currentRow());   //delete it from the widget list
+        delete toRemove;
+
+    }
+}
+
+void MainWindow::on_activationSig_Add_pushButton_clicked()
+{
+    qDebug()<<"Add activation signal button pressed";
+    /*if(selectedTile == 0) return;
+    ui->activationSignals_table->insertRow(ui->activationSignals_table->rowCount());
+    QTableWidgetItem * item1 = new QTableWidgetItem("0");
+    QComboBox * item2 = new QComboBox();
+    item2->addItem("X");
+    item2->addItem("Y");
+    item2->addItem("-X");
+    item2->addItem("-Y");
+    tablePair *pair = new tablePair(item1, item2);
+    selectedTile->activationSignals[currentSide].append(pair);
+    ui->activationSignals_table->setItem(ui->activationSignals_table->rowCount() - 1, 0, selectedTile->activationSignals[currentSide].last()->first);
+    ui->activationSignals_table->setCellWidget(ui->activationSignals_table->rowCount() - 1, 1, selectedTile->activationSignals[currentSide].last()->second);
+*/
+
+}
+
+void MainWindow::on_activationSig_Remove_pushButton_clicked()
+{
+    qDebug()<<"Remove activation signal button pressed";
+  /*  if(selectedTile == 0 || ui->activationSignals_table->rowCount() == 0) return;
+    tablePair * toRemove = selectedTile->activationSignals[currentSide].at(ui->activationSignals_table->currentRow());
+    selectedTile->activationSignals[currentSide].removeAt(ui->activationSignals_table->currentRow());
+    ui->activationSignals_table->takeItem(ui->activationSignals_table->currentRow(), 0);
+    ui->activationSignals_table->takeItem(ui->activationSignals_table->currentRow(), 1);
+    ui->activationSignals_table->removeRow(ui->activationSignals_table->currentRow());
+
+    delete toRemove;*/
+}
+
+void MainWindow::on_transmissionSig_Add_pushButton_clicked()
+{
+    qDebug()<<"Add transmission signal button pressed";
+    //ui->transmissionSignals_table->insertRow(ui->transmissionSignals_table->rowCount());
+}
+
+void MainWindow::on_transmissionSig_Remove_pushButton_clicked()
+{
+    qDebug()<<"Remove transmission signal button pressed";
+    //ui->transmissionSignals_table->removeRow(ui->transmissionSignals_table->currentRow());
+}
+
+void MainWindow::on_tile_comboBox_currentIndexChanged(int index) //when different tile is selected
+{
+    qDebug()<<"Combobox index changed to: "<<index;
+    if(index < 0) return;
+    ui->listWidget->setCurrentRow(index);
+    selectedTile = tiles.at(index);
+}
+
+
+
+void MainWindow::on_listWidget_itemChanged(QListWidgetItem *item)
+{
+    qDebug()<<"Item changed event: "<<item->text();
+    ui->tile_comboBox->removeItem(ui->listWidget->currentRow());
+    ui->tile_comboBox->insertItem(ui->listWidget->currentRow(), item->text());
+}
+
+void MainWindow::on_radioButton_SideX_clicked()
+{
+    qDebug()<<"Side X checked";
+    currentSide = 0;
+    updateEntries(0);
+}
+
+void MainWindow::on_radioButton_SideY_clicked()
+{
+    qDebug()<<"Side Y checked";
+    currentSide = 1;
+    updateEntries(1);
+}
+
+void MainWindow::on_radioButton_Side_X_clicked()
+{
+    qDebug()<<"Side -X checked";
+    currentSide = 2;
+    updateEntries(2);
+}
+
+void MainWindow::on_radioButton_Side_Y_clicked()
+{
+    qDebug()<<"Side -Y checked";
+    currentSide = 3;
+    updateEntries(3);
+}
+
+
+
+void MainWindow::on_activeLabels_listWidget_itemChanged(QListWidgetItem *item)
+{
+    int value = item->text().toInt();
+    qDebug()<<"Active label changed to: "<<value;
+    if(this->strengthFunction.contains(value)) return; //check if current value is present
+    this->strengthFunction[value] = -1;
+    this->strengthFunction[-value] = -1;
+    ui->strength_func_tableWidget->insertRow(ui->strength_func_tableWidget->rowCount());
+    ui->strength_func_tableWidget->setItem(ui->strength_func_tableWidget->rowCount() - 1,
+                                           0, new QTableWidgetItem(QString::number(abs(value))));
+    ui->strength_func_tableWidget->setItem(ui->strength_func_tableWidget->rowCount() - 1,
+                                           1, new QTableWidgetItem("-1"));
+}
+
+void MainWindow::on_inactiveLabels_listWidget_itemChanged(QListWidgetItem *item)
+{
+    int value = item->text().toInt();
+    qDebug()<<"Inactive label changed to: "<<value;
+    if(this->strengthFunction.contains(value)) return; //check if current value is present
+    this->strengthFunction[value] = -1;
+    this->strengthFunction[-value] = -1;
+    ui->strength_func_tableWidget->insertRow(ui->strength_func_tableWidget->rowCount());
+    ui->strength_func_tableWidget->setItem(ui->strength_func_tableWidget->rowCount() - 1,
+                                           0, new QTableWidgetItem(QString::number(abs(value))));
+    ui->strength_func_tableWidget->setItem(ui->strength_func_tableWidget->rowCount() - 1,
+                                           1, new QTableWidgetItem("-1"));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,206 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QListWidget>
+#include <QTableWidget>
+#include <QComboBox>
+#include <QMap>
 namespace Ui {
 class MainWindow;
 }
+
+struct tablePair
+{
+    tablePair(QTableWidgetItem * f, QComboBox * s)
+    {
+        first = f;
+        second = s;
+    }
+
+    ~tablePair()
+    {
+        delete first;
+        delete second;
+    }
+
+    QTableWidgetItem * first;
+    QComboBox * second;
+};
+
+struct a_tile   //struct that will hold all information about a single tile
+{
+    a_tile(QString tileName)
+    {
+        this->Tile = new QListWidgetItem(tileName);
+    }
+
+    ~a_tile()
+    {
+        delete Tile;
+
+       /* for(QList<QListWidgetItem *>::iterator iter = activeLabelsX.begin(); iter != activeLabelsX.end(); iter++)
+        {
+            delete (*iter);
+        }
+
+        for(QList<QListWidgetItem *>::iterator iter = activeLabels_X.begin(); iter != activeLabels_X.end(); iter++)
+        {
+            delete (*iter);
+        }
+
+        for(QList<QListWidgetItem *>::iterator iter = activeLabelsY.begin(); iter != activeLabelsY.end(); iter++)
+        {
+            delete (*iter);
+        }
+
+        for(QList<QListWidgetItem *>::iterator iter = activeLabels_Y.begin(); iter != activeLabels_Y.end(); iter++)
+        {
+            delete (*iter);
+        }
+
+        for(QList<QListWidgetItem *>::iterator iter = inactiveLabelsX.begin(); iter != inactiveLabelsX.end(); iter++)
+        {
+            delete (*iter);
+        }
+
+        for(QList<QListWidgetItem *>::iterator iter = inactiveLabels_X.begin(); iter != inactiveLabels_X.end(); iter++)
+        {
+            delete (*iter);
+        }
+
+        for(QList<QListWidgetItem *>::iterator iter = inactiveLabelsY.begin(); iter != inactiveLabelsY.end(); iter++)
+        {
+            delete (*iter);
+        }
+
+        for(QList<QListWidgetItem *>::iterator iter = inactiveLabels_Y.begin(); iter != inactiveLabels_Y.end(); iter++)
+        {
+            delete (*iter);
+        }
+
+        for(QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> >::iterator iter = activationSignalsX.begin(); iter != activationSignalsX.end(); iter++)
+        {
+            delete (*iter).first;
+            delete (*iter).second;
+        }
+
+        for(QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> >::iterator iter = activationSignals_X.begin(); iter != activationSignals_X.end(); iter++)
+        {
+            delete (*iter).first;
+            delete (*iter).second;
+        }
+
+        for(QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> >::iterator iter = activationSignalsY.begin(); iter != activationSignalsY.end(); iter++)
+        {
+            delete (*iter).first;
+            delete (*iter).second;
+        }
+
+        for(QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> >::iterator iter = activationSignals_Y.begin(); iter != activationSignals_Y.end(); iter++)
+        {
+            delete (*iter).first;
+            delete (*iter).second;
+        }
+
+        for(QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> >::iterator iter = transmissionSignalsX.begin(); iter != transmissionSignalsX.end(); iter++)
+        {
+            delete (*iter).first;
+            delete (*iter).second;
+        }
+
+        for(QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> >::iterator iter = transmissionSignals_X.begin(); iter != transmissionSignals_X.end(); iter++)
+        {
+            delete (*iter).first;
+            delete (*iter).second;
+        }
+
+        for(QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> >::iterator iter = transmissionSignalsY.begin(); iter != transmissionSignalsY.end(); iter++)
+        {
+            delete (*iter).first;
+            delete (*iter).second;
+        }
+
+        for(QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> >::iterator iter = transmissionSignals_Y.begin(); iter != transmissionSignals_Y.end(); iter++)
+        {
+            delete (*iter).first;
+            delete (*iter).second;
+        }
+
+        for(QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> >::iterator iter = initiationSignals.begin(); iter != initiationSignals.end(); iter++)
+        {
+            delete (*iter).first;
+            delete (*iter).second;
+        }*/
+        for(int i = 0; i < 4; i++)
+        {
+            for(QList<QListWidgetItem *>::iterator iter = activeLabels[i].begin(); iter != activeLabels[i].end(); iter++)
+            {
+                 delete (*iter);
+            }
+
+            for(QList<QListWidgetItem *>::iterator iter = inactiveLabels[i].begin(); iter != inactiveLabels[i].end(); iter++)
+            {
+                 delete (*iter);
+            }
+
+            for(QList<tablePair*>::iterator iter = activationSignals[i].begin();
+                iter != activationSignals[i].end(); iter++)
+            {
+                delete (*iter);
+            }
+
+            for(QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> >::iterator iter = transmissionSignals[i].begin();
+                iter != transmissionSignals[i].end(); iter++)
+            {
+                delete (*iter).first;
+                delete (*iter).second;
+            }
+        }
+
+        for(QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> >::iterator iter = initiationSignals.begin();
+            iter != initiationSignals.end(); iter++)
+        {
+            delete (*iter).first;
+            delete (*iter).second;
+        }
+    }
+
+    //Tile entry in the main lists of tiles
+    QListWidgetItem * Tile;
+
+    //Active labels
+    QList<QListWidgetItem *> activeLabels[4];
+    /*QList<QListWidgetItem *> activeLabelsX;
+    QList<QListWidgetItem *> activeLabels_X;
+    QList<QListWidgetItem *> activeLabelsY;
+    QList<QListWidgetItem *> activeLabels_Y;
+*/
+    //Inactive labels
+    QList<QListWidgetItem *> inactiveLabels[4];
+    /*QList<QListWidgetItem *> inactiveLabelsX;
+    QList<QListWidgetItem *> inactiveLabels_X;
+    QList<QListWidgetItem *> inactiveLabelsY;
+    QList<QListWidgetItem *> inactiveLabels_Y;
+*/
+    //Activation Signals
+    QList<tablePair *> activationSignals[4];
+    /*
+    QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> > activationSignalsX;
+    QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> > activationSignals_X;
+    QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> > activationSignalsY;
+    QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> > activationSignals_Y;
+*/
+    //Transmission Signals
+    QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> > transmissionSignals[4];
+    /*
+    QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> > transmissionSignalsX;
+    QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> > transmissionSignals_X;
+    QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> > transmissionSignalsY;
+    QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> > transmissionSignals_Y;
+*/
+    //Initiation Signals
+    QList<QPair<QTableWidgetItem  *, QTableWidgetItem  *> > initiationSignals;
+};
 
 class MainWindow : public QMainWindow
 {
@@ -14,6 +210,7 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void updateEntries(short side);   //update all entries with current tile value
     
 private slots:
 
@@ -27,9 +224,50 @@ private slots:
 
     void on_tabWidget_currentChanged(int index);
 
+    void on_actionExit_triggered();
+
+    void on_activeLabel_Add_pushButton_clicked();
+
+    void on_activeLabel_Remove_pushButton_clicked();
+
+    void on_inactiveLabel_Add_pushButton_clicked();
+
+    void on_inactiveLabel_Remove_pushButton_clicked();
+
+    void on_activationSig_Add_pushButton_clicked();
+
+    void on_activationSig_Remove_pushButton_clicked();
+
+    void on_transmissionSig_Add_pushButton_clicked();
+
+    void on_transmissionSig_Remove_pushButton_clicked();
+
+    void on_tile_comboBox_currentIndexChanged(int index);
+
+
+    void on_listWidget_itemChanged(QListWidgetItem *item);
+
+    void on_radioButton_SideX_clicked();
+
+    void on_radioButton_SideY_clicked();
+
+    void on_radioButton_Side_X_clicked();
+
+    void on_radioButton_Side_Y_clicked();
+
+    void on_listWidget_clicked(const QModelIndex &index);
+
+    void on_activeLabels_listWidget_itemChanged(QListWidgetItem *item);
+
+    void on_inactiveLabels_listWidget_itemChanged(QListWidgetItem *item);
+
 private:
     Ui::MainWindow *ui;
     int currentTile;    //will keep a count of the tiles that were created
+    QList<a_tile *> tiles;
+    a_tile * selectedTile;
+    short currentSide;
+    QMap<int, int> strengthFunction;
 };
 
 #endif // MAINWINDOW_H
